b6078450ad4d9ca088901f123c90fb2c313b77a7
Emmanuel <aghason.emmanuel@gmail.com>
2025-10-20 11:41:56 -0400

    mobile-view

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -82,18 +82,18 @@ ASGI_APPLICATION = 'venexpro.asgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'venexprodb',
-        # 'USER': 'root',
-        # 'PASSWORD': 'Aghason1999',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        # },
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emmidevcodes$venexprodb',
+        'USER': 'emmidevcodes',
+        'PASSWORD': 'Aghason1999',
+        'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
