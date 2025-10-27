183d889f50690b99eba8993c809628f2971be315
Emmanuel <aghason.emmanuel@gmail.com>
2025-10-27 12:15:18 -0400

    Server Working

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY', default="your_dev_secret_key_here") # type: ignor
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', '127.0.0.1:8000', '127.0.0.1:3000']
+ALLOWED_HOSTS = ['www.venexbtc.com', 'venexbtc.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -102,18 +102,18 @@ CHANNEL_LAYERS = {
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'emmidevcodes$venexprodb',
-        # 'USER': 'emmidevcodes',
-        # 'PASSWORD': 'Aghason1999',
-        # 'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
-        # 'PORT': '3306',
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        # },
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emmidevcodes$venexprodb',
+        'USER': 'emmidevcodes',
+        'PASSWORD': 'Aghason1999',
+        'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -222,12 +222,11 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1:8000', 'http://127.0.0.1:3000']
+CORS_ALLOWED_ORIGINS = ['https://www.venexbtc.com', 'https://venexbtc.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
+    'https://www.venexbtc.com',
+    'https://venexbtc.com',
 ]
 
 # Security Settings
@@ -251,6 +250,11 @@ else:
     SECURE_HSTS_SECONDS = 0
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
     SECURE_HSTS_PRELOAD = False
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://www.venexbtc.com",
+]
+
 
 # ===========================
 # EMAIL CONFIGURATION (Zoho)
