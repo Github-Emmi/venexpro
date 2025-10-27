42d21083f5ab087a2d18ed1e59f5e69515a4af25
Emmanuel <aghason.emmanuel@gmail.com>
2025-10-27 12:12:03 -0400

    Server Working

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY', default="your_dev_secret_key_here") # type: ignor
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['www.venexbtc.com', 'venexbtc.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', '127.0.0.1:8000', '127.0.0.1:3000']
 
 # Application definition
 INSTALLED_APPS = [
@@ -102,18 +102,18 @@ CHANNEL_LAYERS = {
 # Database
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'emmidevcodes$venexprodb',
-        'USER': 'emmidevcodes',
-        'PASSWORD': 'Aghason1999',
-        'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'emmidevcodes$venexprodb',
+        # 'USER': 'emmidevcodes',
+        # 'PASSWORD': 'Aghason1999',
+        # 'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
+        # 'PORT': '3306',
+        # 'OPTIONS': {
+        #     'charset': 'utf8mb4',
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        # },
     }
 }
 
@@ -222,11 +222,12 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = ['https://www.venexbtc.com', 'https://venexbtc.com']
+CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1:8000', 'http://127.0.0.1:3000']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://www.venexbtc.com',
-    'https://venexbtc.com',
+    'http://localhost',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
 ]
 
 # Security Settings
