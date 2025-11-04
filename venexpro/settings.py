ad1f27be7bfeb86e735e992173357702351633ba
Emmanuel <aghason.emmanuel@gmail.com>
2025-11-04 03:27:32 -0500

    Added live chat

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -19,16 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY', default="your_dev_secret_key_here") # type: ignore
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True) # type: ignore
+DEBUG = env.bool('DEBUG', default=False) # type: ignore
 
 # PythonAnywhere configuration
 ALLOWED_HOSTS = [
-    # 'www.venexbtc.com',
-    # 'venexbtc.com',
+    'www.venexbtc.com',
+    'venexbtc.com',
     # 'emmidevcodes.pythonanywhere.com',
     # '*.pythonanywhere.com',
-    'localhost',
-    '127.0.0.1',
+    # 'localhost',
+    # '127.0.0.1',
 ]
 
 # Application definition
@@ -111,18 +111,18 @@ CHANNEL_LAYERS = {
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'emmidevcodes$venexprodb',
-        # 'USER': 'emmidevcodes',
-        # 'PASSWORD': 'Aghason1999',
-        # 'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
-        # 'PORT': '3306',
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        # },
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -232,12 +232,12 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    # 'https://www.venexbtc.com',
-    # 'https://venexbtc.com',
+    'https://www.venexbtc.com',
+    'https://venexbtc.com',
     # 'https://emmidevcodes.pythonanywhere.com',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8000',
+    # 'http://localhost:3000',
+    # 'http://127.0.0.1:3000',
+    # 'http://localhost:8000',
     
 ]
 
@@ -264,13 +264,13 @@ else:
     SECURE_HSTS_PRELOAD = False
 
 CSRF_TRUSTED_ORIGINS = [
-    # "https://www.venexbtc.com",
-    # "https://venexbtc.com",
+    "https://www.venexbtc.com",
+    "https://venexbtc.com",
     # "https://emmidevcodes.pythonanywhere.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    # "http://localhost:3000",
+    # "http://127.0.0.1:3000",
+    # "http://localhost:8000",
+    # "http://127.0.0.1:8000",
     
 ]
 
@@ -284,18 +284,18 @@ EMAIL_HOST = "smtp.zoho.com"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False  # must be False when using SSL
-EMAIL_HOST_USER = "venexbtc@venexbtc.com"
-EMAIL_HOST_PASSWORD = "a4siJdTwe4KA"  # your Zoho app password
-DEFAULT_FROM_EMAIL = "venexbtc@venexbtc.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "venexbtc@venexbtc.com")  # your Zoho email address
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "a4siJdTwe4KA")  # your Zoho app password
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "venexbtc@venexbtc.com")
 
 
 # Email recipients
-CONTACT_EMAIL = 'venexbtc@venexbtc.com'
-SUPPORT_EMAIL = 'venexbtc@venexbtc.com'
-ADMIN_EMAIL = 'venexbtc@venexbtc.com'
+CONTACT_EMAIL = os.getenv("CONTACT_EMAIL", "venexbtc@venexbtc.com")
+SUPPORT_EMAIL = os.getenv("SUPPORT_EMAIL", "venexbtc@venexbtc.com")
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "venexbtc@venexbtc.com") 
 
 # Site URL for email templates
-# SITE_URL = ['https://www.venexbtc.com', 'https://venexbtc.com'] # Change to your actual domain
+SITE_URL = ['https://www.venexbtc.com', 'https://venexbtc.com'] # Change to your actual domain
 
 # Authentication settings
 LOGIN_URL = '/login/'
