508089733d0cb6a3f3efa5ff5c5f142f102fdd91
Emmanuel <aghason.emmanuel@gmail.com>
2025-11-04 03:31:48 -0500

    Added live chat

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -113,12 +113,12 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emmidevcodes$venexprodb',
+        'USER': 'emmidevcodes',
+        'PASSWORD': 'Aghason1999',
+        'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
@@ -284,15 +284,15 @@ EMAIL_HOST = "smtp.zoho.com"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False  # must be False when using SSL
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "venexbtc@venexbtc.com")  # your Zoho email address
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "a4siJdTwe4KA")  # your Zoho app password
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "venexbtc@venexbtc.com")
+EMAIL_HOST_USER = "venexbtc@venexbtc.com"
+EMAIL_HOST_PASSWORD = "a4siJdTwe4KA"  # your Zoho app password
+DEFAULT_FROM_EMAIL = "venexbtc@venexbtc.com"
 
 
 # Email recipients
-CONTACT_EMAIL = os.getenv("CONTACT_EMAIL", "venexbtc@venexbtc.com")
-SUPPORT_EMAIL = os.getenv("SUPPORT_EMAIL", "venexbtc@venexbtc.com")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "venexbtc@venexbtc.com") 
+CONTACT_EMAIL = 'venexbtc@venexbtc.com'
+SUPPORT_EMAIL = 'venexbtc@venexbtc.com'
+ADMIN_EMAIL = 'venexbtc@venexbtc.com'
 
 # Site URL for email templates
 SITE_URL = ['https://www.venexbtc.com', 'https://venexbtc.com'] # Change to your actual domain
