0b87a2cbb380813101f661ef91ef0d0ea425bdd3
Emmanuel <aghason.emmanuel@gmail.com>
2025-11-04 11:16:32 -0500

    Now Responsive

--- venexpro/settings.py
+++ venexpro/settings.py
@@ -23,10 +23,10 @@ DEBUG = env.bool('DEBUG', default=True) # type: ignore
 
 # PythonAnywhere configuration
 ALLOWED_HOSTS = [
-    # 'www.venexbtc.com',
-    # 'venexbtc.com',
-    'localhost',
-    '127.0.0.1',
+    'www.venexbtc.com',
+    'venexbtc.com',
+    # 'localhost',
+    # '127.0.0.1',
 ]
 
 # Application definition
@@ -110,18 +110,18 @@ CHANNEL_LAYERS = {
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'emmidevcodes$venexprodb',
-        # 'USER': 'emmidevcodes',
-        # 'PASSWORD': 'Aghason1999',
-        # 'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
-        # 'PORT': '3306',
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        # },
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emmidevcodes$venexprodb',
+        'USER': 'emmidevcodes',
+        'PASSWORD': 'Aghason1999',
+        'HOST': 'emmidevcodes.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
@@ -234,12 +234,11 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    # 'https://www.venexbtc.com',
+    'https://www.venexbtc.com',
     'https://venexbtc.com',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8000',
-    
+    # 'http://localhost:8000',
+    # 'http://127.0.0.1:8000',
+
 ]
 
 # Security Settings
@@ -265,13 +264,11 @@ else:
     SECURE_HSTS_PRELOAD = False
 
 CSRF_TRUSTED_ORIGINS = [
-    # "https://www.venexbtc.com",
-    # "https://venexbtc.com",
+    "https://www.venexbtc.com",
+    "https://venexbtc.com",
     
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    # "http://localhost:8000",
+    # "http://127.0.0.1:8000",
     
 ]
 
@@ -296,9 +293,9 @@ SUPPORT_EMAIL = 'venexbtc@venexbtc.com'
 ADMIN_EMAIL = 'venexbtc@venexbtc.com'
 
 # Site URL for email templates
-# SITE_URL = ['https://www.venexbtc.com', 'https://venexbtc.com'] # Change to your actual domain
+SITE_URL = ['https://www.venexbtc.com', 'https://venexbtc.com'] # Change to your actual domain
  
-SITE_URL = ['http://localhost:8000', 'http://127.0.0.1:8000']
+# SITE_URL = ['http://localhost:8000', 'http://127.0.0.1:8000']
 
 # Authentication settings
 LOGIN_URL = '/login/'
